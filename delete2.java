06936c2ef2190a6a564d06470a63b1969aa3b0f4
Prathish S <prathish472@gmail.com>
2026-05-15 17:59:00 +0530

    Update delete2.java

--- delete2.java
+++ delete2.java
@@ -5,7 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
 public class Delete {
+  
   
     public static void main(String[] args) {
         try {
